39f766d14cd5b1a47ba08dabd7d8a8e3cdc6771c
Yorman Pabon <yormanqp@gmail.com>
2022-09-11 15:42:53 -0500

    Se crea capa de presentacion y servicios

--- Gimnasio.App/GymEnCasa.App.Presentacion/Pages/Error.cshtml.cs
+++ Gimnasio.App/GymEnCasa.App.Presentacion/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace GymEnCasa.App.Servicios.Pages;
+namespace GymEnCasa.App.Presentacion.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
