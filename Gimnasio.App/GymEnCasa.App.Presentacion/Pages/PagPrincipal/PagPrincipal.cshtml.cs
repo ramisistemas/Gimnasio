aeb2918c3b841bec7b0b3f178d423a856f9d4b67
Yorman Pabon <yormanqp@gmail.com>
2022-09-12 16:52:51 -0500

    Creacion Vista Pag Principal

---
+++ Gimnasio.App/GymEnCasa.App.Presentacion/Pages/PagPrincipal/PagPrincipal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class PagPrincipalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
