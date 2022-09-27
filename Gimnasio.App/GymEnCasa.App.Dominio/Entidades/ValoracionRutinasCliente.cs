19a341aa89eb965b80c333149ba3525c3ceb1bbf
ramisistemas <ramiro.cruzado@ingenieros.com>
2022-09-26 22:58:31 -0500

    Validacion-RutinasClientes

--- Gimnasio.App/GymEnCasa.App.Dominio/Entidades/ValoracionRutinasCliente.cs
+++ Gimnasio.App/GymEnCasa.App.Dominio/Entidades/ValoracionRutinasCliente.cs
@@ -1,13 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GymEnCasa.App.Dominio
 {
     public class ValoracionRutinasCliente
     {
         public int Id  {get;set;}
+        [Required]
         public Cliente? Cliente {get;set;}
+        [Required]
         public DificultadEjercicio? DificultadEjercicio {get;set;}
+        [Required]
         public float estatura {get;set;}
+        [Required]
         public float peso {get;set;}
     }
 }
